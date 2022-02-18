291f0258a0cc6a523628cc437e6d645d7e580213
William Denning <69355124+WilliamDenning@users.noreply.github.com>
2022-02-18 17:30:57 +0000

    the code

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ ctx.stroke();
 }
 }
 
-//drawGrid();
+drawGrid();
 
 function getColor()
 {
@@ -106,232 +106,28 @@ this.context.stroke(this.path);
 }
 
 let starSize = canvas.width / 11;
+let strokeColor = 'black';
+let fillColor = 'red';
 
 x = 0;
 y = 0;
 
-/*
-// row 1
-let star_00 = new Star(0, 0, starSize, getColor(), getColor(), ctx);
-star_00.draw();
+const stars = [];
 
-x += (boxWidth * 2);
-let star_02 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_02.draw();
+renderStars();
 
-x += (boxWidth * 2);
-let star_04 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_04.draw();
-
-x += (boxWidth * 2);
-let star_06 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_06.draw();
-
-x += (boxWidth * 2);
-let star_08 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_08.draw();
-
-x += (boxWidth * 2);
-let star_010 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_010.draw();
-
-// row 2
-x = boxWidth;
-y = boxHeight;
-
-let star_11 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_11.draw();
-
-x += (boxWidth * 2);
-let star_13 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_13.draw();
-
-x += (boxWidth * 2);
-let star_15 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_15.draw();
-
-x += (boxWidth * 2);
-let star_17 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_17.draw();
-
-x += (boxWidth * 2);
-let star_19 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_19.draw();
-
-// row 3
-x = 0;
-y += boxHeight;
-
-let star_20 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_20.draw();
-
-x += (boxWidth * 2);
-let star_22 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_22.draw();
-
-x += (boxWidth * 2);
-let star_24 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_24.draw();
-
-x += (boxWidth * 2);
-let star_26 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_26.draw();
-
-x += (boxWidth * 2);
-let star_28 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_28.draw();
-
-x += (boxWidth * 2);
-let star_210 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_210.draw();
-
-// row 4
-x = boxWidth;
-y += boxHeight;
-
-let star_31 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_31.draw();
-
-x += (boxWidth * 2);
-let star_33 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_33.draw();
-
-x += (boxWidth * 2);
-let star_35 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_35.draw();
-
-x += (boxWidth * 2);
-let star_37 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_37.draw();
-
-x += (boxWidth * 2);
-let star_39 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_39.draw();
-
-// row 5
-x = 0;
-y += boxHeight;
-
-let star_40 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_40.draw();
-
-x += (boxWidth * 2);
-let star_42 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_42.draw();
-
-x += (boxWidth * 2);
-let star_44 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_44.draw();
-
-x += (boxWidth * 2);
-let star_46 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_46.draw();
-
-x += (boxWidth * 2);
-let star_48 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_48.draw();
-
-x += (boxWidth * 2);
-let star_410 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_410.draw();
-
-// row 6
-x = boxWidth;
-y += boxHeight;
-
-let star_51 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_51.draw();
-
-x += (boxWidth * 2);
-let star_53 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_53.draw();
-
-x += (boxWidth * 2);
-let star_55 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_55.draw();
-
-x += (boxWidth * 2);
-let star_57 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_57.draw();
-
-x += (boxWidth * 2);
-let star_59 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_59.draw();
-
-// row 7
-x = 0;
-y += boxHeight;
-
-let star_60 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_60.draw();
-
-x += (boxWidth * 2);
-let star_62 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_62.draw();
-
-x += (boxWidth * 2);
-let star_64 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_64.draw();
-
-x += (boxWidth * 2);
-let star_66 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_66.draw();
-
-x += (boxWidth * 2);
-let star_68 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_68.draw();
-
-x += (boxWidth * 2);
-let star_610 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_610.draw();
-
-// row 8
-x = boxWidth;
-y += boxHeight;
-
-let star_71 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_71.draw();
-
-x += (boxWidth * 2);
-let star_73 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_73.draw();
-
-x += (boxWidth * 2);
-let star_75 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_75.draw();
-
-x += (boxWidth * 2);
-let star_77 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_77.draw();
-
-x += (boxWidth * 2);
-let star_79 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_79.draw();
-
-// row 9
-x = 0;
-y += boxHeight;
-
-let star_80 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_80.draw();
-
-x += (boxWidth * 2);
-let star_82 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_82.draw();
-
-x += (boxWidth * 2);
-let star_84 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_84.draw();
-
-x += (boxWidth * 2);
-let star_86 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_86.draw();
-
-x += (boxWidth * 2);
-let star_88 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_88.draw();
-
-x += (boxWidth * 2);
-let star_810 = new Star(x, y, starSize, getColor(), getColor(), ctx);
-star_810.draw();
-*/
+function renderStars()
+{
+  for (let i = 0; i < 100; i++)
+  {
+    if (i % 2 == 0)
+    {
+      stars.push(new Star(-1, -1, starSize, strokeColor, fillColor, ctx));
+    }
+    else
+    {
+      stars.push(null);
+    }
+  }
+  console.log(stars);
+}
